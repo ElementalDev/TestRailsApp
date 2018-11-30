f64d0c53c95d0665f2d06249f0bce8b4e8b732b9
RCollettSG <41951740+RCollettSG@users.noreply.github.com>
2018-11-30 12:07:39 +0000

    Update provision.sh

--- provision.sh
+++ provision.sh
@@ -4,18 +4,21 @@
 echo "------------------------"
 echo "| Updating Packages... |"
 echo "------------------------"
+sleep 0.5s
 sudo apt-get update
 
 # Install Postgres
 echo "--------------------------"
 echo "| Installing Postgres... |"
 echo "--------------------------"
+sleep 0.5s
 sudo apt-get install postgresql postgresql-contrib libpq-dev
 
 # Set up Postgres
 echo "--------------------------"
 echo "| Setting up Postgres... |"
 echo "--------------------------"
+sleep 0.5s
 echo "sudo pkexec --user postgres psql -c 'CREATE ROLE vsts WITH createdb;'"
 sudo pkexec --user postgres psql -c 'CREATE ROLE vsts WITH createdb;'
 echo "sudo pkexec --user postgres psql -c 'ALTER ROLE vsts WITH superuser;'"
@@ -27,18 +30,21 @@ sudo pkexec --user postgres psql -c 'ALTER ROLE vsts WITH login;'
 echo "----------------------------"
 echo "| Installing required gems |"
 echo "----------------------------"
+sleep 0.5s
 gem install bundler pg rails
 
 # Install rails app dependencies
 echo "------------------------------------"
 echo "| Installing Rails dependencies... |"
 echo "------------------------------------"
+sleep 0.5s
 bundle
 
 # Create and migrate rails dbs
 echo "-----------------------------------------------"
 echo "| Creating and migrating data to Rails DBs... |"
 echo "-----------------------------------------------"
+sleep 0.5s
 rails db:create
 rails db:migrate
 
@@ -46,4 +52,5 @@ rails db:migrate
 echo "--------------------"
 echo "| Running tests... |"
 echo "--------------------"
+sleep 0.5s
 rspec spec
